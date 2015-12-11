13428dc1cf1088a2300dbcaeaa54ea42a65b88ed
Louy <louy08@gmail.com>
2015-12-11 19:23:24 +0200

    Fix build command

--- build_pages.sh
+++ build_pages.sh
@@ -2,7 +2,7 @@ NO_LOG=1 node index > NEW_README.md
 
 git checkout gh-pages
 
-rm README.md
+rm README.md || true
 mv NEW_README.md README.md
 git add README.md
 git commit -m "Circle build from $CIRCLE_SHA1"
