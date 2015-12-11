060cf343135a6a21252cfcfc244b3f69ab745605
Louy <louy08@gmail.com>
2015-12-11 19:55:49 +0200

    Fix commit message

--- build_pages.sh
+++ build_pages.sh
@@ -9,7 +9,7 @@ git add index.html
 git config user.email "bot@urbanmassage.com"
 git config user.name "UrbanMassage Bot"
 
-git commit -m "Circle build from $CIRCLE_SHA1\
-\
+git commit -m "Circle build from $CIRCLE_SHA1
+
 https://circleci.com/gh/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/$CIRCLE_BUILD_NUM"
 git push origin gh-pages
