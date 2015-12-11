2ceb1e59eaf1b2ab02b81855a1b90f7eeb9d05a1
Louy <louy08@gmail.com>
2015-12-11 19:17:16 +0200

    Pages script draft

---
+++ build_pages.sh
@@ -0,0 +1,9 @@
+NO_LOG=1 node index > NEW_README.md
+
+git checkout gh-pages
+
+rm README.md
+mv NEW_README.md README.md
+git add README.md
+git commit -m "Circle build from $CIRCLE_SHA1"
+git push origin gh-pages
