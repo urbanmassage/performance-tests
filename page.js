71bd67f059141ed971505b3706776e4e09c388d9
Louy <louy08@gmail.com>
2015-12-11 19:54:59 +0200

    Update html

--- page.js
+++ page.js
@@ -2,6 +2,10 @@ var async = require('async');
 var chalk = require('chalk');
 var fs = require('fs')
 
+function color(name, value) {
+  return '<span class="'+name+'">'+value+'</span>';
+}
+
 async.mapSeries([
   'amqplib',
 
@@ -36,15 +40,27 @@ async.mapSeries([
     b_max = results.reduce(function(a,b) { return Math.min(a, b.max); }, Infinity);
 
   function c(a, b, c) {
-    if (a === c) return chalk.green(a);
-    if (a > b) return chalk.red(a);
-    return chalk.blue(a);
+    if (a === c) return color('green', a);
+    if (a > b) return color('red', a);
+    return color('blue', a);
   }
 
-  var html = '<html><head><title>Performance Tests | Urban Massage</title></head><body><pre>';
+  var html = '<html><head><title>Performance Tests | Urban Massage</title>';
+
+  html += '<style>' +
+            'html {margin: 0; padding: 0; background: #ccc;}' +
+            'body {margin: 0; padding: 20px;}' +
+            'pre {margin: 0 auto; padding: 20px; background: #fff; width: 400px; max-width: 100%}' +
+            '.red {color: red}' +
+            '.cyan {color: cyan}' +
+            '.green {color: green}' +
+            '.blue {color: blue}' +
+          '</style>';
+
+  html += '</head><body><pre>';
 
   results.forEach(function(result) {
-    html += '%s\t in %sms\tmin %s\tavg\t%s max\t%s', chalk.cyan(result.name), c(result.dur, dur, b_dur), c(result.min, min, b_min), c(result.avg, avg, b_avg), c(result.max, max, b_max);
+    html += '%s\t in %sms\tmin %s\tavg\t%s max\t%s', color('cyan', result.name), c(result.dur, dur, b_dur), c(result.min, min, b_min), c(result.avg, avg, b_avg), c(result.max, max, b_max) + '\n';
   });
 
   html += '</pre></body></html>';
