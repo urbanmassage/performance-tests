151cbef990db6957596f3ce30494f88cdbfec925
Louy <louy08@gmail.com>
2015-12-11 19:26:24 +0200

    Add git identity

--- build_pages.sh
+++ build_pages.sh
@@ -5,5 +5,10 @@ git checkout gh-pages
 rm README.md || true
 mv NEW_README.md README.md
 git add README.md
+
+git config user.email "bot@urbanmassage.com"
+git config user.name "UrbanMassage Bot"
+
+
 git commit -m "Circle build from $CIRCLE_SHA1"
 git push origin gh-pages
